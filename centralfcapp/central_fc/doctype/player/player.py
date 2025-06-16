12993410e3d064d6bf3333636d138d3a6d452340
orlando <echolota@sys-nova.com>
2025-06-16 15:02:45 -0500

    cambios

--- centralfcapp/central_fc/doctype/player/player.py
+++ centralfcapp/central_fc/doctype/player/player.py
@@ -4,11 +4,13 @@
 import frappe
 import requests
 from frappe.model.document import Document
+from frappe.utils import nowdate, getdate, add_years
 
 class Player(Document):
     def validate(self):
         self.validate_cedula()
         self.set_full_name()
+        self.juvenil = es_menor_o_igual_18(self.birth_date)
     
     def validate_cedula(self):
         # Validar que el número de cédula solo contenga dígitos
@@ -18,6 +20,31 @@ class Player(Document):
     def set_full_name(self):
         # Crear un nombre completo para mostrar en las listas
         self.full_name = f"{self.last_name_paternal} {self.last_name_maternal} {self.first_name} {self.second_name or ''}"
+    
+
+
+
+
+def es_menor_o_igual_18(fecha_nacimiento):
+    """
+    Verifica si la fecha de nacimiento indica que la persona tiene 18 años o menos.
+
+    :param fecha_nacimiento: La fecha de nacimiento en formato de cadena (YYYY-MM-DD).
+    :return: True si la persona tiene 18 años o menos, False en caso contrario.
+    """
+    if not fecha_nacimiento:
+        return False
+
+    fecha_nacimiento_dt = getdate(fecha_nacimiento)
+    fecha_actual_dt = getdate(nowdate())
+
+    # Calcula la fecha de nacimiento hace 18 años
+    fecha_hace_18_anos = add_years(fecha_actual_dt, -18)
+
+    # Si la fecha de nacimiento es igual o posterior a la fecha_hace_18_anos,
+    # significa que la persona tiene 18 años o menos.
+    return fecha_nacimiento_dt >= fecha_hace_18_anos
+
 
 @frappe.whitelist()
 def get_cedula(identificacion):
